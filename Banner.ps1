4e68c60bf2f55f8c24867321799046a8666f4352
Another-1 <dnp@mail.ru>
2024-01-29 11:11:57 +0300

    Skip Banner if file isn't changed

--- Banner.ps1
+++ Banner.ps1
@@ -3,9 +3,6 @@ $ipfilter_path = 'L:\Software\RT_Adder\ipfiler.dat'
 $ipfilter_source = 'https://bot.keeps.cyou/static/ipfilter.dat'
 
 # Code
-$use_timestamp = 'N'
-Write-Output 'Подгружаем функции'
-. "$PSScriptRoot\_functions.ps1"
 
 Test-Version ( $PSCommandPath | Split-Path -Leaf )
 Test-Version ( '_functions.ps1' )
@@ -15,28 +12,32 @@ if ( -not ( [bool](Get-InstalledModule -Name PsIni -ErrorAction SilentlyContinue
     Install-Module -Name PsIni -Scope CurrentUser -Force
 }
 
-If ( -not ( Test-path "$PSScriptRoot\_settings.ps1" ) ) {
+If ( -not ( Test-Path "$PSScriptRoot\_settings.ps1" ) ) {
     Set-Preferences
 }
 else { . "$PSScriptRoot\_settings.ps1" }
 
-Write-Output 'Читаем настройки Web-TLO'
-
-$ini_path = $tlo_path + '\data\config.ini'
-$ini_data = Get-IniContent $ini_path
-
-$clients = Get-Clients
-
 Write-Output 'Скачиваем файл'
-Invoke-WebRequest -Uri $ipfilter_source -OutFile $ipfilter_path
-
-foreach ( $client_key in $clients.Keys ) {
-    Initialize-Client $clients[$client_key]
-    Write-Output ( 'Обновляем фильтр в клиенте ' + $clients[$client_key].Name )
-    Switch-Filtering $clients[$client_key] $false
-    Start-Sleep -Seconds 1
-    Switch-Filtering $clients[$client_key] $true
- }
-
- Write-Output 'Готово'
+Invoke-WebRequest -Uri $ipfilter_source -OutFile ( $ipfilter_path -replace '\..+?$', '.new' )
+if ( ( Get-FileHash -Path $ipfilter_path ).Hash -ne ( Get-FileHash -Path ( $ipfilter_path -replace '\..+?$', '.new' ) ).Hash ) {
+    Write-Output 'Файл обновился, перечитываем'
+    $use_timestamp = 'N'
+    Write-Output 'Подгружаем функции'
+    . "$PSScriptRoot\_functions.ps1"
+    Write-Output 'Читаем настройки Web-TLO'
+    $ini_path = $tlo_path + '\data\config.ini'
+    $ini_data = Get-IniContent $ini_path
+    $clients = Get-Clients
+    foreach ( $client_key in $clients.Keys ) {
+        Initialize-Client $clients[$client_key]
+        Write-Output ( 'Обновляем фильтр в клиенте ' + $clients[$client_key].Name )
+        Switch-Filtering $clients[$client_key] $false
+        Start-Sleep -Seconds 1
+        Switch-Filtering $clients[$client_key] $true
+    }
+}
+else {
+    Write-Output 'Файл не изменился'
+}
+Write-Output 'Готово'
  
